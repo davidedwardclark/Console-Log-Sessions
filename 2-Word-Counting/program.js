3b386b6ae9eb7238bd720100f678743ca6e34d41
David Clark <davidedwardclark@gmail.com>
2015-12-03 23:36:43 -0800

    take string count words

--- 2-Word-Counting/program.js
+++ 2-Word-Counting/program.js
@@ -1,2 +1,21 @@
 // Word Counting
 
+var fs = require('fs');
+var filename = process.argv[2];
+var output = {};
+
+function countWordsInString(string) {
+	var arrayOfWords = string.split(" ");
+	arrayOfWords.forEach(function(element, index, array) {
+		if (output[element]) {
+			output[element] = output[element] + 1;
+		} else {
+			output[element] = 1;
+		}
+	});
+	return output;
+}
+
+fs.readFile(filename, 'utf8', function(error, data) {
+	console.log(countWordsInString(data));
+});
