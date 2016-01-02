01a8e2518d571bfc6b21e49d537ab97e5d15834e
David Clark <davidedwardclark@gmail.com>
2016-01-01 18:44:55 -0800

    hangman stuff

---
+++ 3-Hangman/program.js
@@ -0,0 +1,6 @@
+// Experiment with this tech:
+// React: https://facebook.github.io/react/
+// Learn ES6: https://leanpub.com/understandinges6/read/
+// Compile to ES5: http://babeljs.io/
+// Package Management: http://webpack.github.io/
+// Linting: http://eslint.org/ & http://jscs.info/
