5e594c8be978743a6f40ff6567f21c01ea8c4b89
David Clark <davidedwardclark@gmail.com>
2016-01-08 00:07:09 -0800

    add hint

--- 3-Hangman/program.js
+++ 3-Hangman/program.js
@@ -4,3 +4,4 @@
 // Compile to ES5: http://babeljs.io/
 // Package Management: http://webpack.github.io/
 // Linting: http://eslint.org/ & http://jscs.info/
+// Implement as much as you can: https://medium.com/@ryanflorence/welcome-to-future-of-web-application-delivery-9750b7564d9f#.iz0pdlubg
