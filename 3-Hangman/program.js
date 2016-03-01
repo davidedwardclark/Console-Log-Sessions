acafc507f5104beb139c6ba1113744e5e1eacd17
David Clark <davidedwardclark@gmail.com>
2016-03-01 00:25:21 -0800

    final commit most likely

--- 3-Hangman/program.js
+++ 3-Hangman/program.js
@@ -22,16 +22,25 @@
 	'use strict';
 
 	var settingsOption = document.querySelector('.settings');
+	var wordContainer = document.querySelector('#word');
+	var missesContainer = document.querySelector('#incorrectletters');
+	var directionsContainer = document.querySelector('#directions');
+	var gameStarted = false;
+	var word;
+	var wordArray;
+	var wordLength;
+	var enteredUniqueLetters = [];
+	var enteredUniqueMatchingLetters = [];
+	var enteredNonMatchingLetters = [];
+	var guesses = 6;
+	var lost = false;
+	var won = false;
 
 	settingsOption.addEventListener('click', function(e) {
 		var level = e.target.innerHTML.toLowerCase();
 		changeScreen('game');
 		gameSetup(level);
 	}, false);
-
-	function gameSetup(level) {
-		console.log('Difficulty level: ', level);
-	}
 
 	function changeScreen(screen) {
 		var homescreen = document.querySelector('#homescreen');
@@ -43,6 +52,91 @@
 			homescreen.className = '';
 			gamescreen.className = 'screen-on';
 		}
+	}
+
+	var words = {
+		easy: ["package", "guesses","letters"],
+		medium: ["jocks", "quick","junky"],
+		hard: ["the", "dog", "car"]
+	};
+
+	function gameSetup(level) {
+		word = pickWord(level);
+		console.log("the word:", word);
+		wordArray = word.split('');
+		wordLength = wordArray.length;
+		for (var i = 0; i < wordLength; i++) {
+			enteredUniqueMatchingLetters.push('&nbsp;');
+		}
+		updateWordContainer();
+		gameStarted = true;
+	}
+
+	function pickWord(level) {
+		var wordLevel = words[level];
+		var word = wordLevel[Math.floor(Math.random() * wordLevel.length)];
+		return word;
+	}
+
+	document.onkeypress = function(e) {
+		if (gameStarted && !lost && !won) {
+			e = e || window.event;
+			var letter = String.fromCharCode(e.keyCode);
+			if (/[a-z]/.test(letter)) {
+				if (!enteredUniqueLetters.contains(letter)) {
+					enteredUniqueLetters.push(letter);
+					if (wordArray.contains(letter)) {
+						for (var i = 0; i < wordLength; i++) {
+							if (letter === word.split('')[i]) {
+								directionsContainer.innerHTML = "Nice one";
+								enteredUniqueMatchingLetters[i] = letter;
+								updateWordContainer();
+							}
+						}
+					} else {
+						enteredNonMatchingLetters.push(letter);
+						directionsContainer.innerHTML = "This letter does not match";
+						updateMissContainer();
+					}
+				} else {
+					directionsContainer.innerHTML = "This letter has already been entered";
+				}
+			}
+		}
+	};
+
+	function updateWordContainer() {
+		var htmlSnippet = '';
+		for (var i = 0; i < enteredUniqueMatchingLetters.length; i++) {
+			htmlSnippet += '<span>' + enteredUniqueMatchingLetters[i] + '</span>';
+		}
+		wordContainer.innerHTML = htmlSnippet;
+		if (!enteredUniqueMatchingLetters.contains('&nbsp;')) {
+			won = true;
+			directionsContainer.innerHTML = "You win";
+		}
+	}
+
+	function updateMissContainer() {
+		guesses = guesses - 1;
+		var htmlSnippet = '';
+		for (var i = 0; i < enteredNonMatchingLetters.length; i++) {
+			htmlSnippet += enteredNonMatchingLetters[i] + ', ';
+		}
+		missesContainer.innerHTML = htmlSnippet;
+		if (guesses === 0) {
+			lost = true;
+			directionsContainer.innerHTML = "You lose";
+		}
+	}
+
+	Array.prototype.contains = function(needle) {
+		for (var i in this) {
+			if (this[i] === needle) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 })();
