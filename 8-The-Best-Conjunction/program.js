d62075da15902453b564952498c525a080a14bb5
David Clark <davidedwardclark@gmail.com>
2017-05-20 01:44:08 -0700

    takes input, lists conjunctions

--- 8-The-Best-Conjunction/program.js
+++ 8-The-Best-Conjunction/program.js
@@ -2,8 +2,10 @@ var fetchUrl = require('fetch').fetchUrl;
 var readline = require('readline-sync');
 var url = 'http://www-personal.umich.edu/~jlawler/wordlist';
 var input = readline.question('Please enter a word: ');
-var minSize = 2;
+var minSize = 3;
 var matchArray = [];
+var uniqueArray = [];
+var masterArray = [];
 
 fetchUrl(url, function (error, meta, body) {
     var words = body.toString().split('\n');
@@ -13,10 +15,15 @@ fetchUrl(url, function (error, meta, body) {
         for (j = 0; j < input.length - 1; j++) {
             for (k = j; k < input.length - 1; k++) {
                 var subString = input.substring(j, minSize + k);
-                if (word === subString) {
-                    console.log(subString);
+                if (word === subString && input.length !== word.length && subString.length >= minSize) {
+                    matchArray.push(subString);
                 }
             }
         }
     }
+    uniqueArray = matchArray.filter(function(item, pos, self) {
+        return self.indexOf(item) == pos;
+    });
+    masterArray.push(uniqueArray);
+    console.log(masterArray);
 });
