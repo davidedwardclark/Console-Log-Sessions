c6eac648068b1058e09484ad30f0d8f673b5e467
David Clark <daviedwardclark@gmail.com>
2022-04-08 19:46:33 -0700

    Letter Value Sum program

--- 11-Letter-Value-Sum/program.js
+++ 11-Letter-Value-Sum/program.js
@@ -9,7 +9,49 @@
 // lettersum("excellent") => 100
 // lettersum("microspectrophotometries") => 317
 
+var input = "microspectrophotometries";
+var convert_input_to_array;
+var add_it_up = 0;
+var output;
 
+var alphabet = {
+  'a': 1,
+  'b': 2,
+  'c': 3,
+  'd': 4,
+  'e': 5,
+  'f': 6,
+  'g': 7,
+  'h': 8,
+  'i': 9,
+  'j': 10,
+  'k': 11,
+  'l': 12,
+  'm': 13,
+  'n': 14,
+  'o': 15,
+  'p': 16,
+  'q': 17,
+  'r': 18,
+  's': 19,
+  't': 20,
+  'u': 21,
+  'v': 22,
+  'w': 23,
+  'x': 24,
+  'y': 25,
+  'z': 26
+};
+
+convert_input_to_array = input.split('');
+
+output = convert_input_to_array.forEach(function(x) {
+  add_it_up = add_it_up + alphabet[x]
+});
+
+output = add_it_up;
+
+console.log(output);
 
 
 
