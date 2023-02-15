48cead10ad227b35048653368239eb1d08d8d192
David Clark <daviedwardclark@gmail.com>
2023-02-15 09:46:01 -0800

    learning python

---
+++ 13-Learning-Python/hello-world.py
@@ -0,0 +1,15 @@
+# Hello World
+# print("Hello world")
+
+# Get a website and print text
+# import requests
+# resp = requests.get("https://davidedwardclark.com")
+# html = resp.text
+# print(html)
+
+# Inline comment spacing = 2
+# print("#blah")  # Like this.
+
+# Variable types
+# print(type("Hello, world"))
+# print(type(5))
