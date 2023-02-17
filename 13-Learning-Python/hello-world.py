31ac66c796fad3053d34f61953c4cdd8f23eba55
David Clark <daviedwardclark@gmail.com>
2023-02-16 22:58:40 -0800

    new stuff

--- 13-Learning-Python/hello-world.py
+++ 13-Learning-Python/hello-world.py
@@ -13,3 +13,99 @@
 # Variable types
 # print(type("Hello, world"))
 # print(type(5))
+
+# Strings
+# single_quote_string = 'blah, I said "blahahaha" to him'
+# double_quote_string = "hah"
+# print(single_quote_string)
+# print(double_quote_string)
+# print(len(single_quote_string))
+
+# Line Lengths
+# Each line of Python code should contain no more than 79 characters.
+
+# Long Strings
+# long_string = "This planet has - or rather had - a problem, which was this: most of the people living on it were unhappy for pretty much of the time. Many solutions were suggested for this problem, but most of these were largely concerned with the movements of small green pieces of paper, which is odd because on the whole it wasn't the small green pieces of paper that were unhappy."
+# print(long_string)
+
+# String Manipulation
+# string1 = "blah1"
+# string2 = "blah2"
+# string3 = string1 + " " + string2
+# print(string3)
+
+# String Indexing
+# string1 = "abcdefghijklmnop"
+# print(string1[0])
+# print(string1[1])
+# print(string1[-1])
+
+# String Slicing
+# flavor = "apple pie"
+# print(flavor[3:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
